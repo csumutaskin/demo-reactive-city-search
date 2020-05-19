ec1bc507f86d19aa2dc0181623cb5a71dfa399a8
csumutaskin <umutaskin@gmail.com>
2020-05-19 13:32:19 +0300

    Initial commit

---
+++ src/main/java/com/csumut/demo/reactivecitysearch/SpringBootReactiveRestDemo1UsingWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.csumut.demo.reactivecitysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveRestDemo1UsingWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveRestDemo1UsingWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csumut/demo/reactivecitysearch/SpringBootReactiveRestDemo1UsingWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csumut.demo.reactivecitysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveRestDemo1UsingWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
