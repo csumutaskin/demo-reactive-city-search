3caa83c65a3266dca08b44b39d3ad131b8c6f3bb
csumutaskin <umutaskin@gmail.com>
2020-05-19 19:28:31 +0300

    A simple rest end is written, bootstrap class name is altered, javadocs are added.

--- src/main/java/com/csumut/demo/reactivecitysearch/ReactiveRestDemoApplication.java
+++ src/main/java/com/csumut/demo/reactivecitysearch/ReactiveRestDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootReactiveRestDemo1UsingWebFluxApplication {
+public class ReactiveRestDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactiveRestDemo1UsingWebFluxApplication.class, args);
+		SpringApplication.run(ReactiveRestDemoApplication.class, args);
 	}
 
 }
